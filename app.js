c50f9260a7a5180b840a41dab2946cdc0ec7c584
Ronald803 <ronaldblancobalboa@gmail.com>
2024-11-17 21:27:05 -0400

    Install cors and add it to app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes/mainRoutes");
 const config = require("./config");
 const connectDB = require("./db");
@@ -6,6 +7,7 @@ const connectDB = require("./db");
 const createApp = () => {
   const app = express();
   connectDB(config.dbUrl);
+  app.use(cors());
   app.use(express.json());
   router(app);
   return app;
